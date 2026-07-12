47df84d3ad93ee310ed677abcf4e1f3ad98ba5ae
suthat16 <southathhvs16@gmail.com>
2026-07-13 06:33:51 +0700

    Import useMemo at the top of Reports.jsx to resolve Uncaught ReferenceError useMemo is not defined

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { db } from '../utils/db';
 import Portal from './Portal';
 
